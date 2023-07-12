e14e974f8652770a920a9666083f66d0596480a8
praveen <praveen11017kumar@gmail.com>
2023-07-12 15:21:13 +0530

    added lodash, async functionality

--- training-concepts/src/app/dashboard/user-management/user-list/user-list.component.ts
+++ training-concepts/src/app/dashboard/user-management/user-list/user-list.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { UsersService } from '../services/users.service';
 import { StorageService } from 'src/app/shared/services/storage.service';
+import { Observable, Subscription } from 'rxjs';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-user-list',
@@ -19,22 +21,22 @@ export class UserListComponent {
     private formBuilder: FormBuilder,
     private storageService: StorageService){ }
   
-  userList=[{
-    id:'',
-    employee_name:'',
-    employee_salary:'',
-    employee_age:''
-  }];
+  userList:Observable<any> | undefined;
+
+  userDetailsSubscription: Subscription | undefined;
+  locationSubscription: Subscription | undefined;
+  userIdSubscription : Subscription | undefined;
+  currDate: Date | undefined;
 
   ngOnInit(){
-    this.userList = [];
     this.userInfoForm = this.formBuilder.group({
       username: ['',[Validators.required,Validators.minLength(8)]],
       salary: ['',[Validators.required,Validators.minLength(10)]],
       age: ['', [Validators.required,Validators.email]],
     });
 
-    this.storageService.userLocation$.subscribe((res)=>{
+    this.currDate = new Date();
+    this.locationSubscription = this.storageService.userLocation$.subscribe((res)=>{
       this.userLocation = res;
     });
     
@@ -42,22 +44,20 @@ export class UserListComponent {
   }
 
   getUserList(){
-    this.usersService.getUsersList().subscribe(res =>{
-      console.log(res);
-      this.userList = res?.data;
-    });
+   this.userList =  this.usersService.getUsersList();
+   let date = moment(this.currDate).format('DD/MM/yyyy');
+   console.log(date);
   }
 
   onEditClick(user:any){
    this.showUserDetailsForm = true;
    this.userId = user.id;
-   this.usersService.getUserById(user.id).subscribe((res)=>{
+   this.userIdSubscription = this.usersService.getUserById(user.id).subscribe((res)=>{
     console.log(res);
     this.patchValuesToForm(res?.data)
    },(err:any)=>{
       console.log(err);
       this.patchValuesToForm(user);
-      
    })
   }
 
@@ -90,6 +90,11 @@ export class UserListComponent {
   onReset(){
   }
 
+  ngOnDestroy(){
+    this.userDetailsSubscription?.unsubscribe();
+    this.locationSubscription?.unsubscribe();
+    this.userIdSubscription?.unsubscribe();
+  }
   
 
 }
