45b9bab1d8034575699cb3ba9822208df39eaa7c
praveen <praveen11017kumar@gmail.com>
2023-07-17 17:21:37 +0530

    changes in interview screen

--- training-concepts/src/app/dashboard/interview-ques/interview-ques.component.ts
+++ training-concepts/src/app/dashboard/interview-ques/interview-ques.component.ts
@@ -9,38 +9,38 @@ export class InterviewQuesComponent {
   coursesInfo = [
     {
       title: 'Angular',
-      description: 'Angular is a component-based framework.',
+      description: 'Angular is a component-based framework.A collection of well-integrated libraries that cover a wide variety of features, including routing, forms management, client-server communication, and more.',
       courseImage: '../../../../assets/images/ang.png',
       link:'https://github.com/sudheerj/angular-interview-questions',
       cheatSheetLink: 'https://hackr.io/blog/angular-cheat-sheet'
     },
     {
       title: 'Javascript',
-      description: 'Angular is a component-based framework,',
+      description: 'JavaScript (JS) is a lightweight interpreted (or just-in-time compiled) programming language with first-class functions. While it is most well-known as the scripting language for Web pages.',
       courseImage: '../../../../assets/images/java-script.jpg',
       link:'https://github.com/sudheerj/javascript-interview-questions',
       cheatSheetLink: 'https://quickref.me/javascript.html'
     },
     {
       title: 'HTML',
-      description: 'Angular is a component-based framework,',
+      description: 'HTML is the standard markup language for documents designed to be displayed in a web browser. It defines the meaning and structure of web content and assisted by technologies such as CSS and scripting languages such as JS.',
       courseImage: '../../../../assets/images/html-img.png',
       link:'https://www.interviewbit.com/html-interview-questions/',
       cheatSheetLink: 'http://www.simplehtmlguide.com/cheatsheet.php'
     },
     {
       title: 'CSS',
-      description: 'Angular is a component-based framework,',
+      description: 'ACSS stands for Cascading Style Sheets. CSS describes how HTML elements are to be displayed on screen, paper, or in other media. CSS saves a lot of work. It can control the layout of multiple web pages all at once.',
       courseImage: '../../../../assets/images/css.jpg',
       link:'https://www.shiksha.com/online-courses/articles/css-interview-questions-and-answers/',
       cheatSheetLink: 'https://websitesetup.org/css3-cheat-sheet/'
     },
     {
       title: 'Bootstrap',
-      description: 'Angular is a component-based framework,',
+      description: 'Bootstrap is a free and open-source collection of CSS and JavaScript/jQuery code used for creating dynamic websites layout and web applications.',
       courseImage: '../../../../assets/images/bootstrap.jpg',
-      link:'https://www.interviewbit.com/html-interview-questions/',
-      cheatSheetLink: 'https://hackr.io/blog/angular-cheat-sheet'
+      link:'https://www.interviewbit.com/bootstrap-interview-questions/',
+      cheatSheetLink: 'https://hackerthemes.com/bootstrap-cheatsheet/#form-control-lg'
     }
   ]
 }
